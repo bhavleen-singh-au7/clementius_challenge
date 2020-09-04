005781f0cb6a6bd190c321d867bf67167d278f0e
bhavleen-singh-au7 <singhbhavleen3@gmail.com>
2020-09-04 18:49:01 +0530

    Moving to delete after that edit is remaining

--- api/server.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config({ path: "./config.env" });
-
-const app = require("./app");
-
-const port = process.env.PORT;
-app.listen(port, () => {
-  console.log(`CONNECTED TO : ${port}`);
-});
