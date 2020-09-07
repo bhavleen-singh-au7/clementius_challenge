4375569818772973a32121d0592253dbd4c7e52e
bhavleen-singh-au7 <singhbhavleen3@gmail.com>
2020-09-07 09:21:53 -0700

    Landing updated

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -9,8 +9,8 @@ const Landing = () => {
         <div className="col-lg-6 text-center my-5">
           <h1 className="mb-5">Welcome to TEST App</h1>
           <div className="py-5">
-            <Link to="/create" className="btn btn-outline-primary mr-5 my-5">Create</Link>
-            <Link to="/view" className="btn btn-outline-secondary">View</Link>
+            <Link to="/create" className="btn btn-outline-primary mr-5 my-5 px-5">Create</Link>
+            <Link to="/view" className="btn btn-outline-secondary px-5">View</Link>
           </div>
 
         </div>
