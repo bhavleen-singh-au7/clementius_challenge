380a02d7cd666782e11d52af36b80b4cd5f9699f
bhavleen-singh-au7 <singhbhavleen3@gmail.com>
2020-09-07 10:28:58 -0700

    Not happenning

--- client/src/pages/Views/ViewItem.jsx
+++ client/src/pages/Views/ViewItem.jsx
@@ -1,9 +1,12 @@
-import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { edit, trash } from "../../redux/actions/view";
+import {
+  edit,
+  trash,
+} from "../../redux/actions/view";
 import Moment from "react-moment";
-// import EditCreate from "../EditCreate"
+import EditView from "../../components/EditView";
 
 const ViewItem = ({
   edit,
@@ -14,53 +17,70 @@ const ViewItem = ({
     lastName,
     email,
     dob,
-    shortBio
+    shortBio,
   },
-  showActions
-}) => (
+  showActions,
+}) => {
+  const Editing = false;
+
+  // const editMode = (e) => {
+  //   e.preventDefault()
+  //   <EditView />
+  // }
+
+  return (
     <tr>
+      {Editing} ? (
+      <EditView />
+      ) : (
       <td>{firstName}</td>
       <td>{lastName}</td>
       <td>{email}</td>
       <td>
-        <Moment format="DD/MM/YYYY">{dob}</Moment>
+        <Moment format="DD/MM/YYYY">
+          {dob}
+        </Moment>
       </td>
       <td>{shortBio}</td>
       <td>
-        {showActions &&
+        {showActions && (
           <Fragment>
             <button
-              onClick={e => edit(_id)}
+              onClick={(e) =>
+                editMode(e)
+              }
               type="button"
-              className="btn btn-light rounded-circle shadow m-1"
-            >
+              className="btn btn-light rounded-circle shadow m-1">
               <i className="fa fa-edit"></i>
             </button>
             {"  "}
             <button
-              onClick={e => trash(_id)}
+              onClick={(e) =>
+                trash(_id)
+              }
               type="button"
-            className="btn btn-danger text-light rounded-circle shadow m-1"
-            >
+              className="btn btn-danger text-light rounded-circle shadow m-1">
               <i className="fa fa-trash"></i>
             </button>
           </Fragment>
-        }
+        )}
       </td>
+      )
     </tr>
   );
+};
 
 ViewItem.defaultProps = {
-  showActions: true
+  showActions: true,
 };
 
 ViewItem.propTypes = {
   view: PropTypes.object.isRequired,
   edit: PropTypes.func.isRequired,
-  trash: PropTypes.func.isRequired
+  trash: PropTypes.func.isRequired,
 };
 
-export default connect(
-  null,
-  { edit, trash }
-)(ViewItem);
+export default connect(null, {
+  edit,
+  trash,
+})(ViewItem);
