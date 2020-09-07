f246fdfcc22f57dd50efd5a40add33d7f6f537d5
bhavleen-singh-au7 <singhbhavleen3@gmail.com>
2020-09-07 09:23:59 -0700

    Form for edit

--- client/src/pages/EditCreate.jsx
+++ client/src/pages/EditCreate.jsx
@@ -1,12 +1,12 @@
 import React, { Fragment, useState } from 'react';
-import { createUser } from "../redux/actions/createUser";
-import { setAlert } from "../redux/actions/alert";
+// import { createUser } from "../redux/actions/createUser";
+// import { setAlert } from "../redux/actions/alert";
 import { connect } from "react-redux";
 import PropTypes from 'prop-types';
-import { Redirect } from "react-router-dom";
+// import { Redirect } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 
-const Create = ({ createUser }) => {
+const Create = ({ updateUser }) => {
 
   const [formData, setFormData] = useState({
     firstName: "",
@@ -20,12 +20,10 @@ const Create = ({ createUser }) => {
 
   const handleChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const handleSubmit = async e => {
+  const handleUpdate = async e => {
     e.preventDefault();
 
-    createUser({ firstName, lastName, email, dob, shortBio });
-
-    toast.success("User Created");
+    updateUser({ firstName, lastName, email, dob, shortBio });
 
     setFormData({
       ...formData,
@@ -39,18 +37,10 @@ const Create = ({ createUser }) => {
 
   return (
     <Fragment>
-      <div className="col-lg-4 offset-lg-4 col-md-6 offset-md-3 col-sm-8 offset-sm-2">
-        <ToastContainer />
-        <div className="text-center mx-2 p-5 shadow-lg color rounded-lg">
-          <h1 className="display-3 font-weight-bold">User</h1>
-
-          <h3 className="py-3">
-            <i className="fa fa-user-plus"></i>
-            {"  "}
-            Create Your Account
-         </h3>
-
-          <form onSubmit={e => handleSubmit(e)}>
+          <form 
+          className="form-inline"
+          onSubmit={e => handleUpdate(e)}
+          >
             <div className="form-group">
               <input
                 type="text"
@@ -112,11 +102,14 @@ const Create = ({ createUser }) => {
                 onChange={e => handleChange(e)}
               />
             </div>
-            <input type="submit" className="btn
-             btn-outline-primary btn-block my-4" value="Create" />
+            <button
+              onClick={e => update(_id)}
+              type="button"
+              className="btn btn-outline-success rounded-circle shadow m-1"
+            >
+              <i className="fa fa-check"></i>
+            </button>
           </form>
-        </div>
-      </div>
     </Fragment>
   );
 };
