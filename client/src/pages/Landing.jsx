1ab35eb3b2dbfa98bc74c20080f260b2e77503f9
bhavleen-singh-au7 <singhbhavleen3@gmail.com>
2020-11-07 11:51:55 +0530

    Updated On November2020

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -7,7 +7,7 @@ const Landing = () => {
     <div className="container">
       <div className="row">
         <div className="col-lg-6 text-center my-5">
-          <h1 className="mb-5">Welcome to TEST App</h1>
+          <h1 className="mb-5">Welcome to Full Stack App</h1>
           <div className="py-5">
             <Link to="/create" className="btn btn-outline-primary mr-5 my-5 px-5">Create</Link>
             <Link to="/view" className="btn btn-outline-secondary px-5">View</Link>
